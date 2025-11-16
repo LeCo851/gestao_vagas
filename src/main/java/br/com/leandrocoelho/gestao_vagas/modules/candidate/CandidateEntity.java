cd3453f6475852605c13e665ff2a393957f92460
Leandro Coelho <leandro.coelho@edu.unirio.br>
2025-11-16 17:30:29 -0300

    Formatação de mensagem de validação e criação das validaçÕes

---
+++ src/main/java/br/com/leandrocoelho/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,30 @@
+package br.com.leandrocoelho.gestao_vagas.modules.candidate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\s+", message = "O campo [username] não deve conter espaços em branco" )
+    private String username;
+
+    @Email(message = "O campo email deve conter um email válido")
+    private String email;
+
+    @Length(min = 10, max=100, message = "O campo deve conter entre 10 e 100 caracteres")
+    private String password;
+    private String description;
+    private String curriculum;
+
+
+}
